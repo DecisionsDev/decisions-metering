6fa911a007edc2adfce55d8733525ba2c7a8503c
laurent grateau <laurent.grateau@fr.ibm.com>
2020-11-13 18:04:18 +0100

    First draft without https.

---
+++ docker/scripts/installFeatures.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+# Install the driver for Derby
+echo "Install the feature list for ODM on Liberty"
+ROOTFEATUREDIR=/opt/wlppackage
+PACKAGELIST="servlet-4.0 springBoot-2.0"
+ls /opt/wlppackage/
+if [ ! -d $ROOTFEATUREDIR/features ]; then
+  mkdir -p $ROOTFEATUREDIR
+  echo "Downloading features list : $PACKAGELIST"
+  installUtility download $PACKAGELIST --location=$ROOTFEATUREDIR;
+fi
+installUtility install $PACKAGELIST --from=$ROOTFEATUREDIR
