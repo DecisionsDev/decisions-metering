492f8511155106aa4bb4976498f93175e0059d82
laurent grateau <laurent.grateau@fr.ibm.com>
2021-02-02 14:19:21 +0100

    Fix cache

--- preparebuild.sh
+++ preparebuild.sh
@@ -7,7 +7,7 @@ cd ../
 ODM_FILE_NAME=odm-distrib-${ODM_VERSION}.zip
 #if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
 echo "ODM distribution: Starting download..."
-if [! -f "$HOME/.cache/$ODM_FILE_NAME"]; then
+if [ ! -f "$HOME/.cache/$ODM_FILE_NAME" ]; then
     ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
     curl $ODM_ZIP_URL -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PWD}" -o $ODM_FILE_NAME
     mv $ODM_FILE_NAME $HOME/.cache/
