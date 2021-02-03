9e3e606f518c566d1ee1eb99de117e97511af58c
laurent grateau <laurent.grateau@fr.ibm.com>
2021-02-03 18:18:01 +0100

    no message

--- docker/testing/test.sh
+++ docker/testing/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Perfom et first execution
-
+set -e
 RESULT=$(curl -X POST -u "odmAdmin:odmAdmin" http://localhost:9080/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0 -b "loanvalidation.json" -T 'loanvalidation.json' -H "Content-Type: application/json; charset=UTF-8" -v)
 
 echo $RESULT | grep "The borrower's SSN" 
@@ -10,8 +10,14 @@ if [ $? -ne 0 ]; then
     exit 1;
 fi
 
-hey -D "loanvalidation.json" -n 1000 -m "POST" -H "Authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -T "application/json; charset=UTF-8" http://localhost:9080/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0
+hey -D "loanvalidation.json" -n 3000 -m "POST" -H "Authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -T "application/json; charset=UTF-8" http://localhost:9080/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0
 if [ $? -ne 0 ]; then
     echo "Load seems KO"
     exit 1;
 fi
+
+curl http://localhost:8888/backup --output ilmt.zip
+unzip ilmt.zip -d ilmt
+# Assert the metering files contains expected metrics
+grep "MILLION_MONTHLY_DECISIONS" ilmt/*.slmtag
+grep '0.0.3' ilmt/*.slmtag
