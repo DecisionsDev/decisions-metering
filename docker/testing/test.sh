cc405ad5c0277f8cae288388663bb8d382e98d08
laurent grateau <laurent.grateau@fr.ibm.com>
2021-02-04 08:47:50 +0100

    Add assertion for Runtime.

--- docker/testing/test.sh
+++ docker/testing/test.sh
@@ -12,12 +12,18 @@ fi
 
 hey -D "loanvalidation.json" -n 3000 -m "POST" -H "Authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -T "application/json; charset=UTF-8" http://localhost:9080/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0
 if [ $? -ne 0 ]; then
-    echo "Load seems KO"
-    exit 1;
+    echo "Result of load test seems not good."
 fi
 
 curl http://localhost:8888/backup --output ilmt.zip
 unzip ilmt.zip -d ilmt
 # Assert the metering files contains expected metrics
 grep "MILLION_MONTHLY_DECISIONS" ilmt/*.slmtag
-grep '0.0.3' ilmt/*.slmtag
+if [ $? -ne 0 ]; then
+    echo "Cannot find the expected value MILLION_MONTHLY_DECISIONS in the ILMT File. Metrics for runtime"
+fi
+grep "<Value>0.003</Value>" ilmt/4ee9e1b0d7aba2e118b0be1649928b14.slmtag
+if [ $? -ne 0 ]; then
+    echo "Cannot find the expected value 0.003 in the ILMT File. (3000 executions"
+fi
+
