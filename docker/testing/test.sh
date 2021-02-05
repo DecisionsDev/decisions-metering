3ce240918c6078508e6c7e38c1a62ed8bea03d30
laurent grateau <laurent.grateau@fr.ibm.com>
2021-02-05 10:19:41 +0100

    Refactor test

--- docker/testing/test.sh
+++ docker/testing/test.sh
@@ -1,29 +1,60 @@
 #!/bin/bash
-
+set -e
 # Perfom et first execution
+function loadRuntime() {
+    ServerURL=$1
+    NbRequest=$2
+    echo "---------------------------------------------"
+    echo "- Executing on $NbRequest on Server URL : $1 "
+    echo "---------------------------------------------"
+    RESULT=$(curl -X POST -u "odmAdmin:odmAdmin" $ServerURL/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0 -b "loanvalidation.json" -T 'loanvalidation.json' -H "Content-Type: application/json; charset=UTF-8" -v)
 
-RESULT=$(curl -X POST -u "odmAdmin:odmAdmin" http://localhost:9080/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0 -b "loanvalidation.json" -T 'loanvalidation.json' -H "Content-Type: application/json; charset=UTF-8" -v)
+    echo $RESULT | grep "The borrower's SSN" 
+    if [ $? -ne 0 ]; then
+        echo "Cannot verify payload"
+        exit 1;
+    fi
 
-echo $RESULT | grep "The borrower's SSN" 
-if [ $? -ne 0 ]; then
-    echo "Cannot verify payload"
-    exit 1;
-fi
+    # Execution for the 
+    hey -D "loanvalidation.json" -n $NbRequest -m "POST" -H "Authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -T "application/json; charset=UTF-8" $ServerURL/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0
+    if [ $? -ne 0 ]; then
+        echo "Result of load test seems not good."
+        exit 1;
+    fi
+    echo "---------------------------------------------"
+    echo "- End of execution "
+    echo "---------------------------------------------"
+}
 
-hey -D "loanvalidation.json" -n 3000 -m "POST" -H "Authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -T "application/json; charset=UTF-8" http://localhost:9080/DecisionService/rest/test_deployment/1.0/loan_validation_with_score_and_grade/1.0
-if [ $? -ne 0 ]; then
-    echo "Result of load test seems not good."
-fi
+function assertResult() {
+    Metric=$1
+    Ratio=$2
+    ComputeExecution=$Ration*1000000
+    # Assert the metering files contains expected metrics
+    grep "$Metric" ilmt/*.slmtag
+    if [ $? -ne 0 ]; then
+        echo "Cannot find the expected value $Metric in the ILMT File. Metrics for runtime"
+        exit 1;
+    fi
+    grep "<Value>$Ratio</Value>" ilmt/*.slmtag
+    if [ $? -ne 0 ]; then
+        echo "Cannot find the expected value $Metric in the ILMT File - $ComputeExecution executions"
+        exit 1;
+    fi
+}
 
-curl http://localhost:8888/backup --output ilmt.zip
-unzip ilmt.zip -d ilmt
-# Assert the metering files contains expected metrics
-grep "MILLION_MONTHLY_DECISIONS" ilmt/*.slmtag
-if [ $? -ne 0 ]; then
-    echo "Cannot find the expected value MILLION_MONTHLY_DECISIONS in the ILMT File. Metrics for runtime"
-fi
-grep "<Value>0.003</Value>" ilmt/*.slmtag
-if [ $? -ne 0 ]; then
-    echo "Cannot find the expected value 0.003 in the ILMT File. (3000 executions"
-fi
+# Assert Runtime of the RES Console
+loadRuntime "http://localhost:9080" "2000"
+# Assert Runtime of the Decision Service.
+loadRuntime "http://localhost:9090" "3000"
 
+$(rm -R ilmt ilmt.zip ; true)
+curl -k https://localhost:9999/backup --output ilmt.zip
+unzip -n ilmt.zip -d ilmt
+
+assertResult "MILLION_MONTHLY_DECISIONS" "0.003"
+echo "RESULT : $?"
+
+assertResult "MILLION_MONTHLY_DECISIONS" "0.002"
+echo "RESULT : $?"
+assertResult "THOUSAND_MONTHLY_ARTIFACTS" "0.097"
