5d4ad4cb9ce8e328384180a83f2e3c1de2866306
laurent grateau <laurent.grateau@fr.ibm.com>
2021-02-16 14:08:12 +0100

    no message

--- packagehelm.sh
+++ packagehelm.sh
@@ -4,6 +4,5 @@ echo "start package helm material..."
 echo "current build directory:"
 pwd
 mkdir index && cd index && helm package ../charts/ibm-odm-metering-service 
-#- helm repo index --url https://github.com/lgrateau/decisions-metering/releases/download/$TRAVIS_TAG/ibm-odm-metering-service-20.0.0.tgz
-echo "TRAIS TAG :" $TRAVIS_TAG
-helm repo index --url 'https://github.com/lgrateau/decisions-metering/releases/download/'$TRAVIS_TAG'/ibm-odm-metering-service-20.0.0.tgz' ./
+
+helm repo index --url 'https://github.com/lgrateau/decisions-metering/releases/download/'$TRAVIS_TAG' ./
