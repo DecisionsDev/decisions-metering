cded4082b85b472f926e490de8beeb5f74cb66fc
laurent grateau <laurent.grateau@fr.ibm.com>
2021-03-19 17:17:16 +0100

    Fix helm packaging build

--- packagehelm.sh
+++ packagehelm.sh
@@ -3,6 +3,6 @@ set -e
 echo "start package helm material..."
 echo "current build directory:"
 pwd
-mkdir index && cd index && helm package ../charts/ibm-odm-metering-service 
+mkdir index && cd index && helm package ../charts/ibm-odm-metering 
 
 helm repo index --url 'https://github.com/lgrateau/decisions-metering/releases/download/'$TRAVIS_TAG ./
