b17b2c67b0997257c70f10f2bbc379311f9080c1
laurent grateau <laurent.grateau@fr.ibm.com>
2021-03-19 16:23:21 +0100

    Fix travis build after renaming.

--- installPrerequisites.sh
+++ installPrerequisites.sh
@@ -7,7 +7,7 @@ chmod a+x downloadGhelRelease.sh && ./downloadGhelRelease.sh "$GHELTOKEN" "$CVLI
 chmod a+x cv && sudo mv cv /usr/local/bin && cv version
 # Verifying Helm Charts
 echo "- Linting Helm Charts "
-cv lint helm charts/ibm-odm-metering-service -o $PWD/charts/ibm-odm-metering-service/tests/lintOverride.yaml
+cv lint helm charts/ibm-odm-metering -o $PWD/charts/ibm-odm-metering/tests/lintOverride.yaml
 echo "- Helm charts format is OK "
 echo "-- Preping the environment for building the metering service..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
