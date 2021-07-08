5fe4b48d6d8c74f55698657df18d87d98234af84
Pierre-Yves Lochou <pylochou@fr.ibm.com>
2021-07-08 15:48:52 +0200

    Full chain--first try (it won't work)

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 set -e
-echo "start building metering docker image service..."
+
+echo "Build metering docker image service..."
 
 mvn install -DskipTests=true -Dodm.home=$PWD/../install
+
 docker-compose build
