5afd80edff647d8e85b099270244967c0cad306b
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2022-02-01 18:03:22 +0100

    Pyl ghactions (#32)
    * Test id
    
    * Try to pass an option to the container
    
    * Check rights
    
    * Removed working-directory
    
    * Download cv and lint Helm chart
    
    * Add debug
    
    * Add GHE_TOKEN from secret
    
    * Test older version of cv
    
    * Fix or ignore errors for cv 2.6.0
    
    * Added ephemeral-storage limits for ConfigMap and removed useless lint overrides
    
    * Added Helm packaging
    
    * Fixed index file name
    
    * Download ODM dist
    
    * Make sure ${HOME}/.cache exists
    
    * Full chain--first try (it won't work)
    
    * Add ODMIMAGEVERSION to job's env
    
    * Moved env declaration
    
    * How to get env variables?
    
    * Try to be less verbose
    
    * Try to keep env variables
    
    * Log into Docker
    
    * Debug
    
    * Forgot to re-add env vars
    
    * No need for this env var
    
    * Test login without sudo
    
    * Preserve env for sudo
    
    Co-authored-by: Pierre-Yves Lochou <pylochou@fr.ibm.com>

---
+++ download-cv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+GH_API=https://github.ibm.com/api/v3
+GH_REPO=${GH_API}/repos/IBMPrivateCloud/content-verification
+GH_LATEST=${GH_REPO}/releases/tags/v2.6.0
+AUTH="Authorization: token ${GHE_TOKEN}"
+
+response=$(curl --header "${AUTH}" --silent ${GH_LATEST})
+id=$(echo "${response}" | jq '.assets[2].id' | tr -d '"')
+name=$(echo "${response}" | jq '.assets[2].name' | tr -d '"')
+GH_ASSET=${GH_REPO}/releases/assets/${id}
+
+curl --location --header "${AUTH}" --header "Accept: application/octet-stream" --output ${name} --silent ${GH_ASSET}
+
+tar xvzf ${name}
