58237535f88dd6e28470b3a9a6e4852ba50f4515
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2022-02-01 16:31:05 +0100

    Update download-odm.sh

--- download-odm.sh
+++ download-odm.sh
@@ -11,7 +11,7 @@ ODM_FILE_NAME=odm-distrib-${ODM_VERSION}.zip
 echo "ODM distribution: Starting download..."
 if [ ! -f "$HOME/.cache/$ODM_FILE_NAME" ]; then
     ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-    curl ${ODM_ZIP_URL} --output ${ODM_FILE_NAME} --silent --user "${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}"
+    curl ${ODM_ZIP_URL} --output ${ODM_FILE_NAME} --user "${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}"
     cat ${ODM_FILE_NAME} 
     mv ${ODM_FILE_NAME} ${HOME}/.cache/
     echo "ODM distribution: Download finished."
