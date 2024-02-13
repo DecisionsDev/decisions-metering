6fed7c790de51b4a7102d39cb07893ebded36699
laurent grateau <laurent.grateau@fr.ibm.com>
2024-02-13 16:41:24 +0100

    Fix github action

--- docker/testing/test.sh
+++ docker/testing/test.sh
@@ -56,7 +56,7 @@ $(rm -R ilmt ilmt.zip ; true)
 curl -k -v https://localhost:9999/backup --output ilmt.zip
 unzip -n ilmt.zip -d ilmt
 echo $(cat ilmt/*.* ; true)
-assertResult "MILLION_MONTHLY_DECISIONS" "0.002"
+assertResult "MILLION_MONTHLY_DECISIONS" "0.005"
 echo "RESULT : $?"
 
 assertResult "THOUSAND_MONTHLY_ARTIFACTS" "0.079"
