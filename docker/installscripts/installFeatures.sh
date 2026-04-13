b6d09330319a77b9973c70e54bce5b81790abfb0
Mathias Mouly <mathias.mouly@fr.ibm.com>
2026-04-13 16:16:41 +0200

    move to featureUtility

--- docker/installscripts/installFeatures.sh
+++ docker/installscripts/installFeatures.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 echo "Install the feature list for ODM on Liberty"
-PACKAGELIST="servlet-6.0 springBoot-3.0 transportSecurity-1.0"
+PACKAGELIST="servlet-4.0 springBoot-2.0 transportSecurity-1.0"
 featureUtility installFeature $PACKAGELIST --verbose --acceptLicense --noCache --verify=skip
