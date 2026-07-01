5fa8070dd2175ae548ced7006d6f90255aa0bb88
mmouly <mathias.mouly@fr.ibm.com>
2026-07-01 15:34:39 +0200

    upgrade to spring-boot-406 and spring-707

--- service/src/main/java/com/ibm/decision/metering/ilmt/service/application/MeteringServiceApplication.java
+++ service/src/main/java/com/ibm/decision/metering/ilmt/service/application/MeteringServiceApplication.java
@@ -29,7 +29,7 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
