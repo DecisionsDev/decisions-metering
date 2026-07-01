cddf1b973aad8d063bbeb5b1c1333e4c0820656a
mmouly <mathias.mouly@fr.ibm.com>
2026-07-01 15:41:09 +0200

    update docker liberty to springBoot-4.0

--- docker/installscripts/installFeatures.sh
+++ docker/installscripts/installFeatures.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 echo "Install the feature list for ODM on Liberty"
-PACKAGELIST="servlet-6.0 springBoot-3.0 transportSecurity-1.0"
+PACKAGELIST="servlet-6.0 springBoot-4.0 transportSecurity-1.0"
 featureUtility installFeature $PACKAGELIST --verbose --acceptLicense --noCache --verify=skip
